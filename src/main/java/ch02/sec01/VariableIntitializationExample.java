0aba0e83a7134576aa25716a57da78086ee3274f
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-04-11 20:02:14 +0900

    이것이 자바다 예제

---
+++ src/main/java/ch02/sec01/VariableIntitializationExample.java
@@ -0,0 +1,14 @@
+package ch02.sec01;
+
+public class VariableIntitializationExample {
+    public static void main(String[] args) {
+        //변수 선언
+        int value;
+
+        //연산 결과를 변수 result의 초기값으로 대입
+        //int result = value + 10;   value에 초기값이 존재하지 않아 컴파일 에러가 발생
+
+        //변수 result 값을 읽고 콘솔에 출력
+        //System.out.println(result);
+    }
+}
