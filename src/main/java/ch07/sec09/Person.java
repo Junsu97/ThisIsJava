21729e17f3508866ecc541de05270e7d6dd639b6
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-05-15 18:57:49 +0900

    ----이것이 자바다---- ch7 instanceof 추상 메소드 추상 클래스 -------instanceof----- 변수가 참조하는 객체의 타입을 확인하고자 할때 사용한다 instanceof연산자의 좌항에는 객체가 우항에는 타입이 오고 좌항의 객체가 우항의 타입이 아니면 true를 그렇지 않다면 false를 리턴 -------추상클래스------ 클래스들의 공통적인 필드나 메소드를 추출해서 선언한 클래스를 추상클래스 라고 한다 추상클래스는 실체 클래스의 부모역할을 한다.
    new 연산자를 사용해서 객체를 직접 생성할 수 없고 extends 뒤에만 올수 있다.
    
    ------추상 메소드-------
    추상메소드는 추상클래스 내에서만 선언할 수 있고, 메소드 실행내용인 중괄호가 없다.
    
    추상메소드는 자식클래스의 공통 메소드라는 것만 정의할뿐 실행내용을 가지지 않는다.
    
    추상메소드는 반드시 자식클래스에서 재정의해서 실행 내용을 채워야한다.

---
+++ src/main/java/ch07/sec09/Person.java
@@ -0,0 +1,15 @@
+package ch07.sec09;
+
+public class Person {
+    //필드선언
+    public String name;
+
+    //생성자 선언
+    public Person(String name){
+        this.name = name;
+    }
+
+    public void wolk(){
+        System.out.println("걷습니다.");
+    }
+}
