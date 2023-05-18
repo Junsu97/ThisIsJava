33ec9d9f42772a83c3cad3cc7bc2461bcf14deb8
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-05-18 16:25:42 +0900

    ----이것이 자바다---- ch8 interface의 정적메소드 interface의 private 메소드
    interface의 private 메소드
    Java 9 부터 사용가능
    
    interface의 정적 메소드
    인터페이스 자체의 기능을 위해 선언한다.
    정적메소드는 구현객체가 없어도 인터페이스만으로 호출할 수 있고
    public 을 생략하더라도 자동으로 컴파이로가정에서 붙는 것이 차이점이다.
    
    정적 메소드의 실행부를 작성할 때 주의할 점은 상수필드를 제외한 추상메소드,디폴트메소드,private메소드 등을 호출할 수 없다는 것이다.

---
+++ src/main/java/ch08/sec06/Television.java
@@ -0,0 +1,43 @@
+package ch08.sec06;
+
+public class Television implements RemoteControl {
+    //필드
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("Television를 켭니다.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Television를 끕니다.");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        if(volume > ch08.sec04.RemoteControl.MAX_VOLUME){
+            this.volume = ch08.sec04.RemoteControl.MAX_VOLUME;
+        }else if (volume< ch08.sec04.RemoteControl.MIN_VOLUME){
+            this.volume = ch08.sec04.RemoteControl.MIN_VOLUME;
+        }else{
+            this.volume = volume;
+        }
+
+        System.out.println("현재 Television 볼륨: " + volume);
+    }
+
+    //필드 추가선언
+    private int memoryVolume;
+    @Override
+    public void setMute(boolean mute){
+        if(mute){
+            this.memoryVolume = this.volume;
+            System.out.println("무음 처리합니다.");
+            setVolume(RemoteControl.MIN_VOLUME);
+        }else {
+            System.out.println("무음 해제합니다.");
+            setVolume(this.memoryVolume);
+        }
+    }
+}
---
+++ src/main/java/ch08/sec07/ServiceImpl.java
@@ -0,0 +1,6 @@
+package ch08.sec07;
+
+public class ServiceImpl implements Service{
+    //default는 구현클래스가 재정의 할 필요가 없고
+    //static은 객체 소속이 아니기 때문
+}
