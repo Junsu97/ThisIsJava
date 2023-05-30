8f76ca0f2fe69fe8c7a06dfa21026a00149c1259
Junsu97 <87612274+Junsu97@users.noreply.github.com>
2023-05-30 19:29:45 +0900

    ----이것이 자바다---- ch9 익명 객체
    익명 객체는 필드값, 로컬 변수값, 매개변수값으로 주요 이용된다.
    
    익명 객체는 클래스를 상속하거나 인터페이스를 구현해야만 생성할 수 있다..
    클래스를 상속해서 만들 경우 익명 자식 개게라고 하고, 인터페이스를 구현해서 만들경우 익명 구현 객체라고 한다.

---
+++ src/main/java/ch09/sec02/exam01/A.java
@@ -0,0 +1,16 @@
+package ch09.sec02.exam01;
+
+public class A {
+    // 인스턴스 멤버 클래스
+    class B{}
+
+    //인스턴스 필드 값으로 B 객체 대입
+
+    A(){
+        B b = new B();
+    }
+
+    void method(){
+        B b = new B();
+    }
+}
